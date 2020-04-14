0f71897139fff2d2402aa4c4b84c0192f2ab19b9
eai <alexander.enikeev@gmail.com>
2020-04-14 21:27:50 +0300

    create get_currency_rate method

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib import request, parse
 import logging
+import re
 
+
+rates_URL = 'http://www.cbr.ru/currency_base/daily/'
+currency_name = 'USD'
 
 class CurrencyConverterHTTPServer(BaseHTTPRequestHandler):
     def set_response(self):
@@ -11,12 +16,29 @@ class CurrencyConverterHTTPServer(BaseHTTPRequestHandler):
     def do_GET(self):
         logging.info(f'GET request, \nPath: {str(self.path)}\nHeaders:\n{str(self.headers)}\n')
         self.set_response()
-        self.wfile.write(f'GET request for {self.path}'.encode('utf-8'))
+        currency_rate = self.get_currency_rate()
+        if currency_rate[0] is True:
+            self.wfile.write(f'USD rate is {currency_rate[1]}'.encode('utf-8'))
+        else:
+            self.wfile.write(currency_rate[1].encode('utf-8'))
 
-def run(port=8081):
+    def get_currency_rate(self, search_URL=rates_URL, search_currency=currency_name):
+        req = request.Request(search_URL)
+        resp = request.urlopen(req)
+        respData = resp.read()
+        search_pattern = search_currency + r'.*?(\d{2}[,]\d*)<\/td>'
+        paragraphs = re.findall(search_pattern, str(respData))
+        for line in paragraphs:
+            if line:
+                USD_rate = float(line.replace(',', '.'))
+                return True, USD_rate
+        error_msg = f'ERROR: {search_currency} rate not found.'
+        return False, error_msg
+
+def run(server_class=HTTPServer, handler_class=CurrencyConverterHTTPServer, port=8082):
     logging.basicConfig(level=logging.INFO)
     server_address = ('', port)
-    httpd = HTTPServer(server_address, CurrencyConverterHTTPServer)
+    httpd = server_class(server_address, handler_class)
     logging.info('Starting httpd...\n')
     try:
         httpd.serve_forever()
