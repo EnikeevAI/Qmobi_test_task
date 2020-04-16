7e3cb59625b4a0f88c2346ef85fad0b7dd8b6c1f
eai <alexander.enikeev@gmail.com>
2020-04-16 16:14:29 +0300

    POST method added

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ class CurrencyConverterHTTPServer(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
+
     def get_currency_rate(self, search_currency, search_URL=rates_URL):
         req = request.Request(search_URL)
         error_msg = None
@@ -26,48 +27,97 @@ class CurrencyConverterHTTPServer(BaseHTTPRequestHandler):
             error_msg = e
         if error_msg is None:
             respData = resp.read()
-            search_pattern = search_currency + r'.*?(\d{2}[,]\d*)'
+            search_pattern = r'\b'+ search_currency + r'\b.*?(\d{2}[,]\d*)'
             paragraphs = re.findall(search_pattern, str(respData))
             for line in paragraphs:
                 if line:
                     currency_rate = float(line.replace(',', '.'))
                     return True, currency_rate
-            error_msg = f'ERROR 400: {search_currency} rate not found.'
+            error_msg = f'ERROR 400: "{search_currency}" costs not found.'
         return False, error_msg
 
-    def get_query_params(self, path):
-        query_params = str(path).lstrip('/?').split('=')
-        if 'favicon.ico' not in query_params:
-            return query_params
+
+    def get_query_params(self, request_data):
+        error_msg = None
+        if self.command == 'GET':
+            params = str(request_data).lstrip('/?').replace('&', '=').split('=')
+            try:
+                query_params = {
+                    params[i]: params[i + 1] for i in range(0, len(params), 2)}
+            except Exception as e:
+                logging.error(e)
+                error_msg = f'ERROR 400: Invalid request parameters.'
+        elif self.command == 'POST':
+            try:
+                query_params = json.loads(request_data.decode('utf-8'))
+            except Exception as e:
+                logging.error('get_query_params method POST. ' + str(e))
+                error_msg = f'ERROR 400: Invalid request parameters.'
+        if error_msg:
+            return False, error_msg
+        elif 'favicon.ico' not in query_params:
+            return True, query_params
         else:
             return None
+
+    def get_response(self, query_params):
+        response_result = list()
+        for currency, quantity in query_params.items():
+            currency_rate = self.get_currency_rate(currency.upper())   
+            if currency_rate[0]:
+                try:
+                    quantity = abs(int(quantity))
+                except Exception as e:
+                    logging.error('get_response. ' + str(e))
+                    error_msg = f'ERROR 400: Invalid value for '\
+                                'the number of {currency} to transfer.'
+                    return False, error_msg.encode('utf-8')
+                result_value = currency_rate[1]*quantity
+                response = {
+                    currency: {
+                        'current_rate': currency_rate[1],
+                        'requested_value': quantity,
+                        'result_value': result_value}}
+                logging.info(f'Response value is {response}\n')
+                response_result.append((json.dumps(response) + '\n').encode('utf-8'))
+            else:
+                logging.error(str(currency_rate[1]) + '\n')
+                return False, str(currency_rate[1]).encode('utf-8')
+        return True, response_result
 
 
     def do_GET(self):
         logging.info(f'GET request, \nPath: {str(self.path)}\nHeaders:\n{str(self.headers)}\n')
         self.set_response()
         query_params = self.get_query_params(self.path)
-        if query_params:
-            currency_name = query_params[0]
-            currency_rate = self.get_currency_rate(currency_name.upper())   
-            if currency_rate[0]:
-                try:
-                    requested_value = abs(int(query_params[1]))
-                except Exception as e:
-                    logging.error(e + '\n')
-                    error_msg = f"ERROR 400: {e}."
-                    return self.wfile.write(str(error_msg).encode('utf-8'))
-                result_value = currency_rate[1]*requested_value
-                response = {
-                    currency_name: {
-                        'current_rate': currency_rate[1],
-                        'requested_value': requested_value,
-                        'result_value': result_value}}
-                logging.info(f'Response value is {response}\n')
-                self.wfile.write(json.dumps(response).encode('utf-8'))
+        if query_params[0]:
+            response = self.get_response(query_params[1])
+            if response[0]:
+                for result in response[1]:
+                    self.wfile.write(result)        
             else:
-                logging.error(str(currency_rate[1]) + '\n')
-                self.wfile.write(str(currency_rate[1]).encode('utf-8'))
+                self.wfile.write(response[1])    
+        elif query_params[0] == False:
+            self.wfile.write(str(query_params[1]).encode('utf-8'))
+
+    
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length']) 
+        post_data = self.rfile.read(content_length) 
+        logging.info(f'''POST request,\nPath: {str(self.path)}\nHeaders:
+                    \n{str(self.headers)}\nBody:{post_data.decode('utf-8')}\n''')
+        self.set_response()
+        query_params = self.get_query_params(post_data)
+        if query_params[0]:
+            response = self.get_response(query_params[1])
+            if response[0]:
+                for result in response[1]:
+                    self.wfile.write(result)        
+            else:
+                self.wfile.write(response[1])
+        elif query_params[0] == False:
+            self.wfile.write(str(query_params[1]).encode('utf-8')) 
+
 
 def run(server_class=HTTPServer, handler_class=CurrencyConverterHTTPServer, port=8082):
     logging.basicConfig(level=logging.INFO)
@@ -80,6 +130,7 @@ def run(server_class=HTTPServer, handler_class=CurrencyConverterHTTPServer, port
         pass
     httpd.server_close()
     logging.info('Stopping httpd...\n')
+
 
 if __name__ == '__main__':
     run()
